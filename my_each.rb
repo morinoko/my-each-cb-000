1e044570f28426984ddcfe612203143c4bf64094
Felice Forby <feliceforby@gmail.com>
2018-04-14 08:09:56 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -7,19 +7,3 @@ def my_each(array) # put argument(s) here
   end
   array
 end
-
-# Test if block is given
-# def my_each(array) # put argument(s) here
-#   if block_given?
-#     i = 0
-# 
-#     while i < array.length
-#       yield array[i] # Passes the variable into the block called by the method
-#       i += 1
-#     end
-#     array
-#   else
-#     puts "Please use a block!"
-#   end
-# end
-
